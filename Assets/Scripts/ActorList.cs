f66516a060de21d8c6da2bc5419050b853265208
CharlesDev <charlesgamedev@gmail.com>
2023-11-14 16:38:03 -0500

    actor list changes

--- Assets/Scripts/ActorList.cs
+++ Assets/Scripts/ActorList.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Octokit;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActorList : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class ActorEntry
     {
-        
+        public string name;
+        public string gyaml;
     }
 
-    // Update is called once per frame
-    void Update()
+    [HideInInspector]
+    public List<ActorEntry> actors;
+    [SerializeField] TextAsset actorListText;
+    [SerializeField] GameObject actorPrefab;
+    [SerializeField] Transform actorListParent;
+
+    void Start()
     {
-        
+        actors = new List<ActorEntry>();
+
+        foreach (string s in actorListText.text.Split("\n"))
+        {
+            string[] parts = s.Split(",");
+            ActorEntry ae = new ActorEntry()
+            {
+                name = parts[0].Trim(),
+                gyaml = parts[1].Trim()
+            };
+
+            actors.Add(ae);
+        }
+
+        foreach (ActorEntry entry in actors)
+        {
+            GameObject go = Instantiate(actorPrefab, actorListParent);
+
+            Transform c0 = go.transform.GetChild(0);
+            Transform c1 = go.transform.GetChild(1);
+            Transform c2 = go.transform.GetChild(2);
+
+            c0.GetComponent<TMP_Text>().text = entry.name;
+            c1.GetComponent<TMP_Text>().text = entry.gyaml;
+
+            foreach (var pair in ActorManager.Instance.Sprites)
+                if (entry.gyaml.Contains(pair.Key))
+                    c2.GetComponent<Image>().sprite = pair.Value;
+        }
     }
 }
